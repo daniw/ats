83c618140b87d5517852ee0ed156cd0587d9353b
daniw <dani.winz@bluewin.ch>
2015-11-28 16:27:09 +0100

    Axis labled, single plots

--- PLC-Prog/Lab7/matlab/lab7.m
+++ PLC-Prog/Lab7/matlab/lab7.m
@@ -33,6 +33,8 @@ plot(data_off_0_90.time, data_off_0_90.r, data_off_0_90.time, data_off_0_90.y);
 axis([x_min_off_0_90 x_max_off_0_90 y_min_r_y y_max_r_y]);
 title('Step response, ARW off, 0..90');
 legend('r', 'y');
+xlabel('time [s]');
+ylabel('r, y');
 print('-dpdf', 'off_0_90_r_y');
 
 figure(2);
@@ -40,6 +42,8 @@ plot(data_off_0_90.time, data_off_0_90.u, data_off_0_90.time, data_off_0_90.u_sa
 axis([x_min_off_0_90 x_max_off_0_90 y_min_u_usat y_max_u_usat]);
 title('Step response, ARW off, 0..90');
 legend('u', 'u_{sat}');
+xlabel('time [s]');
+ylabel('u, u_{sat}');
 print('-dpdf', 'off_0_90_u_usat');
 
 figure(3);
@@ -47,6 +51,8 @@ plot(data_off_0_90.time, data_off_0_90.p, data_off_0_90.time, data_off_0_90.i, d
 axis([x_min_off_0_90 x_max_off_0_90 y_min_p_i_d y_max_p_i_d]);
 title('Step response, ARW off, 0..90');
 legend('p', 'i', 'd');
+xlabel('time [s]');
+ylabel('p, i, d');
 print('-dpdf', 'off_0_90_p_i_d');
 
 figure(4);
@@ -54,6 +60,8 @@ plot(data_off_90_0.time, data_off_90_0.r, data_off_90_0.time, data_off_90_0.y);
 axis([x_min_off_90_0 x_max_off_90_0 y_min_r_y y_max_r_y]);
 title('Step response, ARW off, 90..0');
 legend('r', 'y');
+xlabel('time [s]');
+ylabel('r, y');
 print('-dpdf', 'off_90_0_r_y');
 
 figure(5);
@@ -61,6 +69,8 @@ plot(data_off_90_0.time, data_off_90_0.u, data_off_90_0.time, data_off_90_0.u_sa
 axis([x_min_off_90_0 x_max_off_90_0 y_min_u_usat y_max_u_usat]);
 title('Step response, ARW off, 90..0');
 legend('u', 'u_{sat}');
+xlabel('time [s]');
+ylabel('u, u_{sat}');
 print('-dpdf', 'off_90_0_u_usat');
 
 figure(6);
@@ -68,6 +78,8 @@ plot(data_off_90_0.time, data_off_90_0.p, data_off_90_0.time, data_off_90_0.i, d
 axis([x_min_off_90_0 x_max_off_90_0 y_min_p_i_d y_max_p_i_d]);
 title('Step response, ARW off, 90..0');
 legend('p', 'i', 'd');
+xlabel('time [s]');
+ylabel('p, i, d');
 print('-dpdf', 'off_90_0_p_i_d');
 
 figure(7);
@@ -75,6 +87,8 @@ plot(data_on_0_90.time, data_on_0_90.r, data_on_0_90.time, data_on_0_90.y);
 axis([x_min_on_0_90 x_max_on_0_90 y_min_r_y y_max_r_y]);
 title('Step response, ARW on, 0..90');
 legend('r', 'y');
+xlabel('time [s]');
+ylabel('r, y');
 print('-dpdf', 'on_0_90_r_y');
 
 figure(8);
@@ -82,6 +96,8 @@ plot(data_on_0_90.time, data_on_0_90.u, data_on_0_90.time, data_on_0_90.u_sat);
 axis([x_min_on_0_90 x_max_on_0_90 y_min_u_usat y_max_u_usat]);
 title('Step response, ARW on, 0..90');
 legend('u', 'u_{sat}');
+xlabel('time [s]');
+ylabel('u, u_{sat}');
 print('-dpdf', 'on_0_90_u_usat');
 
 figure(9);
@@ -89,6 +105,8 @@ plot(data_on_0_90.time, data_on_0_90.p, data_on_0_90.time, data_on_0_90.i, data_
 axis([x_min_on_0_90 x_max_on_0_90 y_min_p_i_d y_max_p_i_d]);
 title('Step response, ARW on, 0..90');
 legend('p', 'i', 'd');
+xlabel('time [s]');
+ylabel('p, i, d');
 print('-dpdf', 'on_0_90_p_i_d');
 
 figure(10);
@@ -96,6 +114,8 @@ plot(data_on_90_0.time, data_on_90_0.r, data_on_90_0.time, data_on_90_0.y);
 axis([x_min_on_90_0 x_max_on_90_0 y_min_r_y y_max_r_y]);
 title('Step response, ARW on, 90..0');
 legend('r', 'y');
+xlabel('time [s]');
+ylabel('r, y');
 print('-dpdf', 'on_90_0_r_y');
 
 figure(11);
@@ -103,6 +123,8 @@ plot(data_on_90_0.time, data_on_90_0.u, data_on_90_0.time, data_on_90_0.u_sat);
 axis([x_min_on_90_0 x_max_on_90_0 y_min_u_usat y_max_u_usat]);
 title('Step response, ARW on, 90..0');
 legend('u', 'u_{sat}');
+xlabel('time [s]');
+ylabel('u, u_{sat}');
 print('-dpdf', 'on_90_0_u_usat');
 
 figure(12);
@@ -110,6 +132,8 @@ plot(data_on_90_0.time, data_on_90_0.p, data_on_90_0.time, data_on_90_0.i, data_
 axis([x_min_on_90_0 x_max_on_90_0 y_min_p_i_d y_max_p_i_d]);
 title('Step response, ARW on, 90..0');
 legend('p', 'i', 'd');
+xlabel('time [s]');
+ylabel('p, i, d');
 print('-dpdf', 'on_90_0_p_i_d');
 
 % With subplots
@@ -119,18 +143,24 @@ plot(data_off_0_90.time, data_off_0_90.r, data_off_0_90.time, data_off_0_90.y);
 axis([x_min_off_0_90 x_max_off_0_90 y_min_r_y y_max_r_y]);
 title('Step response, ARW off, 0..90');
 legend('r', 'y');
+xlabel('time [s]');
+ylabel('r, y');
 
 subplot(3,1,2);
 plot(data_off_0_90.time, data_off_0_90.u, data_off_0_90.time, data_off_0_90.u_sat);
 axis([x_min_off_0_90 x_max_off_0_90 y_min_u_usat y_max_u_usat]);
 title('Step response, ARW off, 0..90');
 legend('u', 'u_{sat}');
+xlabel('time [s]');
+ylabel('u, u_{sat}');
 
 subplot(3,1,3);
 plot(data_off_0_90.time, data_off_0_90.p, data_off_0_90.time, data_off_0_90.i, data_off_0_90.time, data_off_0_90.d);
 axis([x_min_off_0_90 x_max_off_0_90 y_min_p_i_d y_max_p_i_d]);
 title('Step response, ARW off, 0..90');
 legend('p', 'i', 'd');
+xlabel('time [s]');
+ylabel('p, i, d');
 print('-dpdf', 'off_0_90');
 
 figure(14);
@@ -139,18 +169,24 @@ plot(data_off_90_0.time, data_off_90_0.r, data_off_90_0.time, data_off_90_0.y);
 axis([x_min_off_90_0 x_max_off_90_0 y_min_r_y y_max_r_y]);
 title('Step response, ARW off, 90..0');
 legend('r', 'y');
+xlabel('time [s]');
+ylabel('r, y');
 
 subplot(3,1,2);
 plot(data_off_90_0.time, data_off_90_0.u, data_off_90_0.time, data_off_90_0.u_sat);
 axis([x_min_off_90_0 x_max_off_90_0 y_min_u_usat y_max_u_usat]);
 title('Step response, ARW off, 90..0');
 legend('u', 'u_{sat}');
+xlabel('time [s]');
+ylabel('u, u_{sat}');
 
 subplot(3,1,3);
 plot(data_off_90_0.time, data_off_90_0.p, data_off_90_0.time, data_off_90_0.i, data_off_90_0.time, data_off_90_0.d);
 axis([x_min_off_90_0 x_max_off_90_0 y_min_p_i_d y_max_p_i_d]);
 title('Step response, ARW off, 90..0');
 legend('p', 'i', 'd');
+xlabel('time [s]');
+ylabel('p, i, d');
 print('-dpdf', 'off_90_0');
 
 figure(15);
@@ -159,18 +195,24 @@ plot(data_on_0_90.time, data_on_0_90.r, data_on_0_90.time, data_on_0_90.y);
 axis([x_min_on_0_90 x_max_on_0_90 y_min_r_y y_max_r_y]);
 title('Step response, ARW on, 0..90');
 legend('r', 'y');
+xlabel('time [s]');
+ylabel('r, y');
 
 subplot(3,1,2);
 plot(data_on_0_90.time, data_on_0_90.u, data_on_0_90.time, data_on_0_90.u_sat);
 axis([x_min_on_0_90 x_max_on_0_90 y_min_u_usat y_max_u_usat]);
 title('Step response, ARW on, 0..90');
 legend('u', 'u_{sat}');
+xlabel('time [s]');
+ylabel('u, u_{sat}');
 
 subplot(3,1,3);
 plot(data_on_0_90.time, data_on_0_90.p, data_on_0_90.time, data_on_0_90.i, data_on_0_90.time, data_on_0_90.d);
 axis([x_min_on_0_90 x_max_on_0_90 y_min_p_i_d y_max_p_i_d]);
 title('Step response, ARW on, 0..90');
 legend('p', 'i', 'd');
+xlabel('time [s]');
+ylabel('p, i, d');
 print('-dpdf', 'on_0_90');
 
 figure(16);
@@ -179,17 +221,23 @@ plot(data_on_90_0.time, data_on_90_0.r, data_on_90_0.time, data_on_90_0.y);
 axis([x_min_on_90_0 x_max_on_90_0 y_min_r_y y_max_r_y]);
 title('Step response, ARW on, 90..0');
 legend('r', 'y');
+xlabel('time [s]');
+ylabel('r, y');
 
 subplot(3,1,2);
 plot(data_on_90_0.time, data_on_90_0.u, data_on_90_0.time, data_on_90_0.u_sat);
 axis([x_min_on_90_0 x_max_on_90_0 y_min_u_usat y_max_u_usat]);
 title('Step response, ARW on, 90..0');
 legend('u', 'u_{sat}');
+xlabel('time [s]');
+ylabel('u, u_{sat}');
 
 subplot(3,1,3);
 plot(data_on_90_0.time, data_on_90_0.p, data_on_90_0.time, data_on_90_0.i, data_on_90_0.time, data_on_90_0.d);
 axis([x_min_on_90_0 x_max_on_90_0 y_min_p_i_d y_max_p_i_d]);
 title('Step response, ARW on, 90..0');
 legend('p', 'i', 'd');
+xlabel('time [s]');
+ylabel('p, i, d');
 print('-dpdf', 'on_90_0');
 
